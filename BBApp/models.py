10531230efa4c6690b62e4d374168aa12bae84f5
Evaristo R Rivieccio Vega <evaristorivieccio@gmail.com>
2018-09-29 13:37:42 +0200

    Arrancando

---
+++ BBApp/models.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Usuarios(models.Model):
+    choice_text = models.CharField(max_length=200)
+    
+    pub_date = models.DateTimeField('date published')
+
+
+class Cervezas(models.Model):
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+class Pubs(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
