7e945aa20973bd69780fd8d1c1f0cda3b3b38882
Evaristo R Rivieccio Vega <evaristorivieccio@gmail.com>
2018-10-02 20:14:42 +0200

    Modelos v4 mejorado

--- BBApp/models.py
+++ BBApp/models.py
@@ -4,28 +4,20 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-class Usuarios(models.Model):
+class Usuario(models.Model):
     nombre_usuario = models.CharField(max_length=200)
     email = models.EmailField(max_length=70,blank=True)
-class Votos(models.Model):
-    una_estrella = models.IntegerField(default=0)
-    dos_estrella = models.IntegerField(default=0)
-    tres_estrella = models.IntegerField(default=0)
-    cuatro_estrella = models.IntegerField(default=0)
-    cinco_estrella = models.IntegerField(default=0)
-
-    usuarios = models.ManyToManyField(Usuarios)
-
-class Cervezas(models.Model):
+class Votaciones(models.Model):
+    usuario = models.ManyToManyField(Usuario)
+    voto = models.IntegerField(default=0)
+    cerveza = models.ManyToManyField(Cerveza)
+class Cerveza(models.Model):
    	nombre_cerveza = models.CharField(max_length=200)
    	observaciones = models.TextField()
-   	fotos = models.FileField(upload_to='fotos/%Y/%m/%d')
-
-   	puntuacion = models.ManyToManyField(Votos)
-
-class Pubs(models.Model):
+   	foto = models.FileField(upload_to='fotos/%Y/%m/%d')
+class Pertenencia(models.Model):
+    nombre_Pub = models.ManyToManyField(Pub)
+    cerveza = models.ManyToManyField(Cerveza)
+class Pub(models.Model):
     nombre_Pub = models.CharField(max_length=200)
-    direccion_pub = models.CharField(max_length=200)
-
-    Cerveza = models.ManyToManyField(Cervezas)
-    
+    direccion_pub = models.CharField(max_length=200)    
