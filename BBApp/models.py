9deda505855322c429410167f2e93bf63bf0fb15
Evaristo R Rivieccio Vega <evaristorivieccio@gmail.com>
2018-10-02 16:35:37 +0200

    Modelos v3 Constraint

--- BBApp/models.py
+++ BBApp/models.py
@@ -13,10 +13,19 @@ class Votos(models.Model):
     tres_estrella = models.IntegerField(default=0)
     cuatro_estrella = models.IntegerField(default=0)
     cinco_estrella = models.IntegerField(default=0)
+
+    usuarios = models.ManyToManyField(Usuarios)
+
 class Cervezas(models.Model):
    	nombre_cerveza = models.CharField(max_length=200)
    	observaciones = models.TextField()
    	fotos = models.FileField(upload_to='fotos/%Y/%m/%d')
+
+   	puntuacion = models.ManyToManyField(Votos)
+
 class Pubs(models.Model):
     nombre_Pub = models.CharField(max_length=200)
     direccion_pub = models.CharField(max_length=200)
+
+    Cerveza = models.ManyToManyField(Cervezas)
+    
