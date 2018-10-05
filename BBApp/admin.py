ff1b408d8c0222557b97e1ccf86d04993eb2c76b
Evaristo R Rivieccio Vega <evaristorivieccio@gmail.com>
2018-10-05 21:39:24 +0200

    Modelos Finalizados

--- BBApp/admin.py
+++ BBApp/admin.py
@@ -4,3 +4,10 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Usuario,Cerveza,Pub,Votaciones
+
+admin.site.register(Usuario)
+admin.site.register(Cerveza)
+admin.site.register(Pub)
+admin.site.register(Votaciones)
--- BBApp/models.py
+++ BBApp/models.py
@@ -15,21 +15,25 @@ class Usuario(models.Model):
 class Cerveza(models.Model):
    	nombre_cerveza = models.CharField(max_length=200)
    	observaciones = models.TextField()
-   	foto = models.FileField(upload_to='fotos/%Y/%m/%d')
-    voto = models.ManyToManyField(voto, through='Votaciones')
+   	foto = models.FileField(upload_to='BBApp/images/%Y/%m/%d')
 
-    def __str__(self):
-        return self.nombre_cerveza
-
-class Votaciones(models.Model):
-    usuario = models.ManyToManyField(Usuario)
-    voto = models.IntegerField(default=0)
-    cerveza = models.ManyToManyField(Cerveza)
+	def __str__(self):
+		return self.nombre_cerveza
 
 class Pub(models.Model):
     nombre_pub = models.CharField(max_length=200)
     direccion_pub = models.CharField(max_length=200)
-    cerveza = models.ManyToManyField(Cerveza)    
+    cerveza = models.ManyToManyField(Cerveza, through='Votaciones')
+
+def __str__(self):
+		return self.nombre_pub
 
 
+class Votaciones(models.Model):
+    usuario = models.ManyToManyField(Usuario)
+    voto = models.IntegerField(default=0)
+    cerveza = models.ForeignKey(Cerveza, on_delete=models.CASCADE)
+    pub = models.ForeignKey(Pub, on_delete=models.CASCADE)
 
+def __str__(self):
+		return self.voto
