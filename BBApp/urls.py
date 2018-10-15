90cbd44b8a3ff7c94e966d6da52b306c114dbd26
Evaristo R Rivieccio Vega <evaristorivieccio@gmail.com>
2018-10-15 02:01:23 +0200

    Urls, Vistas, Base ¿?

--- BBApp/urls.py
+++ BBApp/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
 from . import views
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from BBApp.views import *
 
 urlpatterns = [
-    url(r'^/usuario/$', UsuarioList.as_view(),name="usuarios_list"),
+    url(r'^usuario/$', UsuarioList.as_view(),name="UsuarioList"),
     url(r'^cerveza/$', CervezaList.as_view()),
     url(r'^pub/$', PubList.as_view()),
     url(r'^votaciones/$', VotacionesList.as_view()),
