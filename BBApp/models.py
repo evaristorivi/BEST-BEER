ffc7d9d1425b5cac584d5e1b997a702c1ff596e1
Evaristo R Rivieccio Vega <evaristorivieccio@gmail.com>
2018-10-17 03:02:52 +0200

    Extensión de modelo de usuairo

--- BBApp/models.py
+++ BBApp/models.py
@@ -2,13 +2,13 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Usuario(models.Model):
     nombre_usuario = models.CharField(max_length=200)
     email = models.EmailField(max_length=70,blank=True)
-
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     def __str__(self):
         return self.nombre_usuario
 
