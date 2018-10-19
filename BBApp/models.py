8dd2c20b135460a6f449004c764319dd0b4c58ee
Evaristo R Rivieccio Vega <evaristorivieccio@gmail.com>
2018-10-19 21:59:18 +0200

    Mejorando signup

--- BBApp/models.py
+++ BBApp/models.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 
 class Usuario(models.Model):
@@ -11,6 +13,13 @@ class Usuario(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     def __str__(self):
         return self.nombre_usuario
+
+@receiver(post_save, sender=User)
+def update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Usuario.objects.create(user=instance)
+    instance.profile.save()
+
 
 class Cerveza(models.Model):
    	nombre_cerveza = models.CharField(max_length=200)
--- BBApp/views.py
+++ BBApp/views.py
@@ -17,22 +17,29 @@ from django.views.generic.edit import DeleteView
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from BBApp.forms import SignUpForm
+
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def home(request):
+    return render(request, 'home.html')
 
 
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
+            user = form.save()
+            user.refresh_from_db()  # load the profile instance created by the signal
+            user.save()
             raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
+            user = authenticate(username=user.username, password=raw_password)
             login(request, user)
             return redirect('home')
     else:
-        form = UserCreationForm()
+        form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
-
 
 
 class UsuarioList(ListView):
